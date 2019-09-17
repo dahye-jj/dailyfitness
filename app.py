e6748fca21dc8b7210079dc24d92d59572c96de6
dahye-jj <55441396+dahye-jj@users.noreply.github.com>
2019-09-17 22:10:59 +0900

    test_3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-   return '성현님 수고하셨습니다!'
+   return render_template('index.html')
 if __name__=='__main__':
     import os
     app.run('0.0.0.0', port=os.environ.get('PORT',5000))
