ac5824d9f15c92145773f6b7d0494e4463473c60
dahye-jj <55441396+dahye-jj@users.noreply.github.com>
2019-09-22 22:36:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    return render_template('index.html')
+
 if __name__=='__main__':
     import os
     app.run('0.0.0.0', port=os.environ.get('PORT',5000))
